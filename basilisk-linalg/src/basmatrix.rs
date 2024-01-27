314ffb12146ffcb26831bfefd6e61618ca65a2bb
Aaditya Aryal <aryalaadi123@gmail.com>
2024-01-27 16:25:43 +0545

    basilisk-linalg/BASMatrixDevice: set up base for openCL/CPU computing choices

--- basilisk-linalg/src/basmatrix.rs
+++ basilisk-linalg/src/basmatrix.rs
@@ -5,6 +5,25 @@
 
 extern crate ocl;
 
+enum BASMatrixDevice {
+    OPENCL,
+    CPU
+}
+
+/*
+    This static variable will determine if the BASMatrix computations 
+    will be done on the CPU or on the GPU with OpenCL
+
+    BASMatrix by will compute everything on the CPU unless dev is mutated by
+    basilisk_linalg::setBasiliskDevice(OPENCL) 
+
+    BY THE WAY, it is SAFE, dw
+*/
+static mut DEV: BASMatrixDevice = BASMatrixDevice::CPU;
+fn set_device(d: BASMatrixDevice) {
+    unsafe { DEV = d };    
+}
+
 struct BASMatrix {
     rows: usize,
     cols: usize,
@@ -13,7 +32,7 @@ struct BASMatrix {
 
 impl BASMatrix {
 
-    fn new(rows: usize, cols: usize) -> Self {
+    pub fn new(rows: usize, cols: usize) -> Self {
         BASMatrix {
             rows,
             cols,
@@ -21,15 +40,15 @@ impl BASMatrix {
         }
     }
 
-    fn get(&self, row: usize, col: usize) -> f64 {
+    pub fn get(&self, row: usize, col: usize) -> f64 {
        	self.data[row * self.cols + col]
     }
 
-    fn set(&mut self, row: usize, col: usize, value: f64) {
+    pub fn set(&mut self, row: usize, col: usize, value: f64) {
        	self.data[row * self.cols + col] = value;
    	}
     
-    fn print(&self) {
+    pub fn print(&self) {
         for i in 0..self.rows {
             for j in 0..self.cols {
                 print!("{} ", self.get(i, j));
@@ -37,18 +56,31 @@ impl BASMatrix {
             println!();
         }
     }
- 
-    fn add(&mut self, to_add: &BASMatrix) -> Result<i32, &str>{
-	    if self.rows == to_add.rows && self.cols == to_add.cols {
-            for i in 0..self.rows  {
-                for j in 0..self.cols {
-                    self.data[i * self.cols + j] = self.data[i * self.cols + j] + to_add.data[i * self.cols + j];
-                }    
+    
+    pub fn add(&mut self, to_add: &BASMatrix) -> Result<i32, &str>{
+        if self.rows == to_add.rows && self.cols == to_add.cols {
+            // NOTE: it is VERY safe :p
+            unsafe {
+                match DEV {
+                    BASMatrixDevice::CPU => self._cpu_add(to_add),
+                    BASMatrixDevice::OPENCL => self._opencl_add(to_add),
+                }
             }
-                Ok(0) // TODO: implement error codes for sucess and failure
-      	}    
-        else {
-           	    Err("BASMatrix: cannot add")
+            Ok(0)
         }
+        else {
+            Err("BASMatrix: cannot add")
+        }
+    }
+
+    fn _cpu_add(&mut self, to_add: &BASMatrix){
+        for i in 0..self.rows  {
+            for j in 0..self.cols {
+                self.data[i * self.cols + j] = self.data[i * self.cols + j] + to_add.data[i * self.cols + j];
+            }    
+        }    
     }	
+    fn _opencl_add(&mut self, to_add: &BASMatrix) {
+        print!("TODO");        
+    }
 }
