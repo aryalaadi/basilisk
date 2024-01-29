770ef2c3c7b772862a6b0242c21a65345904cb59
Aaditya Aryal <aryalaadi123@gmail.com>
2024-01-29 22:33:33 +0545

    basilisk-linalg: BASTensor::set() \nDont even know if it works]

--- basilisk-linalg/src/bastensor.rs
+++ basilisk-linalg/src/bastensor.rs
@@ -7,18 +7,31 @@
 pub struct BASTensor {
     dim: Vec<usize>,
     data: Vec<f64>,
+    size: usize,
 }
 
 impl BASTensor {
     pub fn new(dim: Vec<usize>) -> Self {
-		let mut size = 1;
-		for i in dim.iter() {
-			size *= i;
-		}
-		let data = vec![0.0; size];
-        BASTensor {
-			dim,
-			data
+        let mut size = 1;
+        for i in dim.iter() {
+            size *= i;
         }
+        let data = vec![0.0; size];
+        BASTensor { dim, data, size }
+    }
+
+    pub fn set(&mut self, pos: &[usize], value: f64) -> Result<i32, &str> {
+        if pos.len() != self.dim.len() {
+            return Err("BASTensor: cannot set");
+        }
+        let mut index = 1;
+        for i in pos.iter() {
+            index *= i;
+        }
+        if index != self.size {
+            return Err("BASTensor: cannot set");
+        }
+        self.data[index] = value;
+        Ok(0)
     }
 }
